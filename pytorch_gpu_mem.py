c90620c6305caaa99ac56212935c07d54273249a
backyes <backyes@gmail.com>
2023-06-19 00:21:48 +0800

    Add rmm

---
+++ pytorch_gpu_mem.py
@@ -0,0 +1,7 @@
+# https://github.com/pytorch/pytorch/issues/35901
+# https://discuss.pytorch.org/t/how-does-reserved-in-total-by-pytorch-work/70172/10
+
+
+# Add RMM PyTorch allocator
+https://github.com/rapidsai/rmm/pull/1168/files
+
